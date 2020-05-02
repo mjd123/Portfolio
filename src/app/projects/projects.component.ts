37541fd668d8acfb9c7f44e347e50d288305f44f
wearetelecopicMarcus <marcus@wearetelescopic.co.uk>
2020-05-03 00:24:06 +0100

    improved calculations some styling

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -110,27 +110,27 @@ export class ProjectsComponent implements OnInit {
         const finalProjectTextWidth: any = (projectsText.width * 0.1).toFixed(0);
 
         let tweens = {
-            headerColorFadeOut: TweenMax.to('header, .projects', 2000, {
+            headerColorFadeOut: TweenMax.to('body, .app-content, header, .projects', 300, {
                 backgroundColor: '#f5eee2',
                 //opacity: 1,
                 ease: 'Power1.easeOut',
             }),
-            projectsColorFadeOut: TweenMax.to('.projects ,.app-content, .about ', 2000, {
+            projectsColorFadeOut: TweenMax.to('.projects ,body,.app-content, .about ', 300, {
                 backgroundColor: '#006b64',
                 //opacity: 1,
                 ease: 'Power1.easeOut',
             }),
-            overlayTextChangeTitle: TweenMax.to('.project-container .overlay .title', 2000, {
+            overlayTextChangeTitle: TweenMax.to('.project-container .overlay .title', 300, {
                 fontSize: '30',
                 //opacity: 1,
                 ease: 'Power1.easeOut',
             }),
-            overlayTextChangeSubTitle: TweenMax.to('.project-container .overlay .sub-title', 2000, {
+            overlayTextChangeSubTitle: TweenMax.to('.project-container .overlay .sub-title', 300, {
                 fontSize: '16',
                 //opacity: 1,
                 ease: 'Power1.easeOut',
             }),
-            overlayBorderChangeSubTitle: TweenMax.to('.project-container .overlay', 2000, {
+            overlayBorderChangeSubTitle: TweenMax.to('.project-container .overlay', 300, {
                 border: '1px solid black',
                 //opacity: 1,
                 ease: 'Power1.easeOut',
@@ -145,30 +145,30 @@ export class ProjectsComponent implements OnInit {
                 new TimelineMax().fromTo(
                     '.one',
                     1000,
-                    { x: this.width - this.elementChildren[2].clientWidth / 2, y: '0%' },
+                    { x: this.width - this.elementChildren[2].clientWidth / 2, y: -50 },
                     {
                         scale: 1,
                         z: -1500,
                         x:
                             this.width > 900
-                                ? this.width / 2 - 30 // center with 30px margin
-                                : this.width / 2 - this.elementChildren[2].clientWidth,
-                        y: `${this.width > 420 ? '40%' : '0%'}`,
+                                ? this.width / 2 + 30 // center with 30px margin
+                                : this.width / 2 - this.elementChildren[2].clientWidth + 60 - 30,
+                        y: this.width > 900 ? 80 : 20,
                         ease: 'Linear.easeNone',
                     }
                 ),
                 new TimelineMax().fromTo(
                     '.two',
                     1000,
-                    { x: 0 - this.width - this.elementChildren[3].clientWidth },
+                    { x: 0 - this.width - this.elementChildren[3].clientWidth, y: 0 },
                     {
                         scale: 1,
                         z: -1500,
                         x:
                             this.width > 900
-                                ? this.width / 2 - this.elementChildren[3].clientWidth - 90
-                                : this.width / 2,
-                        y: this.width > 900 ? '15%' : this.elementChildren[2].clientHeight - 80,
+                                ? this.width / 2 - this.elementChildren[3].clientWidth - 30
+                                : this.width / 2 - 60 - 30,
+                        y: this.width > 900 ? 80 : this.elementChildren[2].clientHeight - 30 + 20,
                         ease: 'Linear.easeNone',
                     }
                 ),
@@ -176,33 +176,48 @@ export class ProjectsComponent implements OnInit {
                     '.three',
                     1000,
                     {
-                        x: this.width - this.elementChildren[4].clientWidth / 4,
-                        y: '160%',
+                        x:
+                            this.width > 900
+                                ? this.width - this.elementChildren[4].clientWidth / 4
+                                : this.width - this.elementChildren[4].clientWidth / 10,
+                        y: this.height - 30 + 20,
                     },
                     {
                         scale: 1,
                         z: -1500,
                         x:
                             this.width > 900
-                                ? this.width / 2 - this.elementChildren[4].clientWidth - 90
-                                : this.width / 2 - this.elementChildren[3].clientWidth,
-                        y: this.width > 900 ? '120%' : this.elementChildren[3].clientHeight + 40,
+                                ? this.width / 2 - this.elementChildren[4].clientWidth - 30
+                                : this.width / 2 - this.elementChildren[4].clientWidth + 60 - 30,
+                        y:
+                            this.width > 900
+                                ? this.elementChildren[3].clientHeight + 80 + 30 // 2nd project height + padding top of projects container + margin between 2nd and 3rd project
+                                : this.elementChildren[2].clientHeight +
+                                  this.elementChildren[3].clientHeight -
+                                  60 +
+                                  20,
                         ease: 'Linear.easeNone',
                     }
                 ),
                 new TimelineMax().fromTo(
                     '.four',
                     1000,
-                    { x: this.width + this.elementChildren[5].clientWidth, y: '80%' },
+                    {
+                        x: this.width + this.elementChildren[4].clientWidth * 2,
+                        y: this.height - 50,
+                    },
                     {
                         scale: 1,
                         z: -1500,
-                        x: this.width > 420 ? this.width / 2 - 30 : this.width / 2,
+                        x: this.width > 900 ? this.width / 2 + 30 : this.width / 2 - 60 - 30, // on mobile center -  padding - margin for overlap
                         y:
-                            this.width > 420
-                                ? 0
-                                : this.elementChildren[4].clientHeight +
-                                  this.elementChildren[3].clientHeight,
+                            this.width > 900
+                                ? this.elementChildren[2].clientHeight + 80 + 30 // 1st project height + padding top of projects container + margin between 1st and 4th project
+                                : this.elementChildren[2].clientHeight +
+                                  this.elementChildren[3].clientHeight +
+                                  this.elementChildren[4].clientHeight -
+                                  90 +
+                                  20,
                         ease: 'Linear.easeNone',
                     }
                 ),
@@ -222,8 +237,8 @@ export class ProjectsComponent implements OnInit {
         // header color fade
         new ScrollMagic.Scene({
             triggerHook: 0,
-            duration: 500, // the scene should last for a scroll distance of 100px
-            offset: 200, // start this scene after scrolling for 50px
+            duration: 800, // the scene should last for a scroll distance of 100px
+            offset: this.width > 900 ? 200 : 0, // start this scene after scrolling for 50px
         })
             .addIndicators()
             .setTween(timeline)
@@ -231,11 +246,11 @@ export class ProjectsComponent implements OnInit {
             //.setPin('.projects-text, .projects') // pins the element for the the scene's duration
             .addTo(this.controller); // assign the scene to the controller
 
-        //containeing
+        //container
         new ScrollMagic.Scene({
             triggerElement: '.projects',
-            duration: this.width > 900 ? 3500 : this.height * 2.2, // the scene should last for a scroll distance of 100px
-            offset: this.width > 900 ? 550 : this.height / 2.1, // start this scene after scrolling for 50px
+            duration: this.width > 900 ? this.height * 2.1 : this.height * 1.8, // the scene should last for a scroll distance of 100px
+            offset: this.width > 900 ? 550 : this.height / 2, // start this scene after scrolling for 50px
         })
             .addIndicators()
             .setTween(tweens.startTween)
@@ -246,8 +261,8 @@ export class ProjectsComponent implements OnInit {
         // text scale
         new ScrollMagic.Scene({
             triggerElement: '.projects',
-            duration: this.width > 900 ? 3000 : this.height * 2, // the scene should last for a scroll distance of 100px
-            offset: this.width > 900 ? 550 : 300, // start this scene after scrolling for 50px
+            duration: this.width > 900 ? this.height * 2 : this.height, // the scene should last for a scroll distance of 100px
+            offset: this.width > 900 ? 550 : 600, // start this scene after scrolling for 50px
         })
             .addIndicators()
             .setTween(tweens.startTween)
@@ -258,8 +273,8 @@ export class ProjectsComponent implements OnInit {
         // project image
         new ScrollMagic.Scene({
             triggerElement: '.projects',
-            duration: this.width > 900 ? 2500 : this.height * 2, // the scene should last for a scroll distance of 100px
-            offset: this.width > 900 ? 800 : 300, // start this scene after scrolling for 50px
+            duration: this.width > 900 ? this.height : this.height, // the scene should last for a scroll distance of 100px
+            offset: this.width > 900 ? 800 : 500, // start this scene after scrolling for 50px
         })
             .addIndicators()
             .setTween(tweens.wipeAnimations[0])
@@ -268,8 +283,8 @@ export class ProjectsComponent implements OnInit {
         // project image
         new ScrollMagic.Scene({
             triggerElement: '.projects',
-            duration: this.width > 900 ? 2000 : this.height * 2, // the scene should last for a scroll distance of 100px
-            offset: this.width > 900 ? 800 : 300, // start this scene after scrolling for 50px
+            duration: this.width > 900 ? this.height : this.height, // the scene should last for a scroll distance of 100px
+            offset: this.width > 900 ? 800 : 500, // start this scene after scrolling for 50px
         })
             .addIndicators()
             .setTween(tweens.wipeAnimations[1])
@@ -278,8 +293,8 @@ export class ProjectsComponent implements OnInit {
         // project image
         new ScrollMagic.Scene({
             triggerElement: '.projects',
-            duration: this.width > 900 ? 2200 : this.height * 2, // the scene should last for a scroll distance of 100px
-            offset: this.width > 900 ? 800 : 300, // start this scene after scrolling for 50px
+            duration: this.width > 900 ? this.height : this.height, // the scene should last for a scroll distance of 100px
+            offset: this.width > 900 ? 800 : 500, // start this scene after scrolling for 50px
         })
             .addIndicators()
             .setTween(tweens.wipeAnimations[2])
@@ -288,8 +303,8 @@ export class ProjectsComponent implements OnInit {
         // project image
         new ScrollMagic.Scene({
             triggerElement: '.projects',
-            duration: this.width > 900 ? 2000 : this.height * 2, // the scene should last for a scroll distance of 100px
-            offset: this.width > 900 ? 800 : 300, // start this scene after scrolling for 50px
+            duration: this.width > 900 ? this.height : this.height, // the scene should last for a scroll distance of 100px
+            offset: this.width > 900 ? 800 : 500, // start this scene after scrolling for 50px
         })
             .addIndicators()
             .setTween(tweens.wipeAnimations[3])
@@ -297,7 +312,7 @@ export class ProjectsComponent implements OnInit {
 
         new ScrollMagic.Scene({
             triggerElement: '.about',
-            duration: 500, // the scene should last for a scroll distance of 100px
+            duration: 600, // the scene should last for a scroll distance of 100px
             offset: -200, // start this scene after scrolling for 50px
         })
             .addIndicators()
