6ef3fe0a4239193c516bea96a083efc364fe61b4
wearetelecopicMarcus <marcus@wearetelescopic.co.uk>
2020-05-19 20:32:02 +0100

    build

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const wakeUpDyno = require('wokeDyno.js');
 const app = express();
-const DYNO_URL = 'https://md-port.herokuapp.com/';
+const DYNO_URL = 'https://marcusdenny.dev/';
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/portfolio'));
