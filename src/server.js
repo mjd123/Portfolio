4a309020cdf2cc00e92ccbea3fbb5faabcace8d7
wearetelecopicMarcus <marcus@wearetelescopic.co.uk>
2020-06-02 17:31:55 +0100

    added server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-//const wakeDyno = require('woke-dyno');
+const wakeDyno = require('woke-dyno');
 const app = express();
-//const DYNO_URL = 'https://md-port.herokuapp.com/';
-
+const DYNO_URL = 'https://md-port.herokuapp.com/';
+const port = process.env.PORT || 3000;
 // Serve static files....
 app.use(express.static(__dirname + '/dist/portfolio'));
 
@@ -13,4 +13,10 @@ app.get('/*', function (req, res) {
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 3000);
+//app.listen(process.env.PORT || 3000);
+
+app.listen(port, () => {
+    console.log(port);
+
+    wakeDyno(DYNO_URL).start(); // will start once server starts
+});
